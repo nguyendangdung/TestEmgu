e9dbb6b7288671c2fd90bdd15e173f0d82c38482
Dan Nguyen <ddnguyen0702@hotmail.com>
2017-08-15 10:24:58 +0700

    d

--- Playvideo/Form1.cs
+++ Playvideo/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
 
 namespace Playvideo
 {
 	public partial class Form1 : Form
 	{
+		private bool blnFormClosing;
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			blnFormClosing = true;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var capVideo = new VideoCapture(@"C:\Users\Administrator\Documents\Visual Studio 2015\Projects\TestEmgu\TestEmgu\CarsDrivingUnderBridge.mp4");
+			while (!blnFormClosing)
+			{
+				var imgFrame = capVideo.QueryFrame();
+				if (imgFrame == null)
+				{
+					return;
+				}
+				imageBox1.Image = imgFrame;
+				Application.DoEvents();
+			}
 		}
 	}
 }
