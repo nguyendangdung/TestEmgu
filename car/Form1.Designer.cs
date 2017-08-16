137829a9c1197832b77dbce82435369d181515dd
Dan Nguyen <ddnguyen0702@hotmail.com>
2017-08-16 13:59:53 +0700

    d

--- car/Form1.Designer.cs
+++ car/Form1.Designer.cs
@@ -28,20 +28,49 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.imageBox1 = new Emgu.CV.UI.ImageBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// imageBox1
+			// 
+			this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.imageBox1.Location = new System.Drawing.Point(0, 0);
+			this.imageBox1.Name = "imageBox1";
+			this.imageBox1.Size = new System.Drawing.Size(1028, 522);
+			this.imageBox1.TabIndex = 2;
+			this.imageBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1028, 522);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.imageBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private Emgu.CV.UI.ImageBox imageBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- car/Form1.cs
+++ car/Form1.cs
@@ -16,5 +16,10 @@ namespace car
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
