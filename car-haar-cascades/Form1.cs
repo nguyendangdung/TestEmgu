4c9e1ad4188834b1ef1ba085c67be6f213fb2fa6
Dan Nguyen <ddnguyen0702@hotmail.com>
2017-08-31 09:16:53 +0700

    g

--- car-haar-cascades/Form1.cs
+++ car-haar-cascades/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Emgu.CV;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV.Structure;
 
 namespace car_haar_cascades
 {
@@ -15,6 +17,35 @@ namespace car_haar_cascades
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (var videoCapture = new VideoCapture("cars.mp4"))
+			using (CascadeClassifier cars = new CascadeClassifier("cars.xml"))
+			{
+				while (true)
+				{
+					var frame = videoCapture.QueryFrame();
+					if (frame == null)
+					{
+						break;
+					}
+					using (UMat ugray = new UMat())
+					{
+						CvInvoke.CvtColor(frame, ugray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+
+						CvInvoke.EqualizeHist(ugray, ugray);
+						var faces = cars.DetectMultiScale(ugray);
+						foreach (var face in faces)
+							CvInvoke.Rectangle(frame, face, new Bgr(Color.Red).MCvScalar, 2);
+
+
+						imageBox1.Image = frame;
+						Application.DoEvents();
+					}
+				}
+			}
 		}
 	}
 }
